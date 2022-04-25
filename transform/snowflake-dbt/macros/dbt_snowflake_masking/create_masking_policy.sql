718702a7248b10771b1ef4a08b1891d3f6e07438
Peter Empey <pempey@gitlab.com>
2022-04-25 09:17:18 -0600

    updarted policy roles.

--- transform/snowflake-dbt/macros/dbt_snowflake_masking/create_masking_policy.sql
+++ transform/snowflake-dbt/macros/dbt_snowflake_masking/create_masking_policy.sql
@@ -2,7 +2,8 @@
 
 CREATE MASKING POLICY IF NOT EXISTS "{{ database }}".{{ schema }}.{{ policy }}_{{ data_type }} AS (val {{ data_type }}) 
   RETURNS {{ data_type }} ->
-      CASE WHEN CURRENT_ROLE() IN ('transformer','{{ policy }}') THEN val 
+      CASE WHEN CURRENT_ROLE() IN ('transformer') THEN val  -- Set for specific roles that should always have access
+      CASE WHEN IS_ROLE_IN_SESSION('{{ policy }}') THEN val -- Set for the user to inherit access bases on there roles
       ELSE NULL
       END; 
 
