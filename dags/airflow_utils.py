ae0c73266ccce74977ebcf197908a667f763e7db
Rigerta Demiri <rdemiri@gitlab.com>
2022-07-08 00:12:17 +0200

    Modify command syntax

--- dags/airflow_utils.py
+++ dags/airflow_utils.py
@@ -344,7 +344,7 @@ clone_data_science_repo_cmd = f"""
 
 
 upload_dbt_manifest_to_mcd_cmd = f"""
-    montecarlo import dbt-manifest && 
+    montecarlo import dbt-manifest 
     target/manifest.json --project-name gitlab-analytics"""
 
 # command to exclude models (for test models) in dbt test command
